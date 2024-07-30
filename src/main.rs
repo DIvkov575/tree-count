81e360c14966867568de15993f9f0858ebe3873e
Divkov575 <dmitriy@ivkov.net>
2024-07-30 17:12:00 -0400

    added cli args + vertical foramtting + reformat display implementation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use std::any::Any;
-use std::cmp::Ordering;
 use std::collections::HashMap;
-use std::ffi::{OsStr, OsString};
-use std::fmt::{Debug, Display, Formatter};
+use std::ffi::{OsString};
+use std::fmt::{Debug, Display, Error, Formatter};
 use std::fs::{File, read_dir};
-use std::hash::Hash;
-use std::io::{BufRead, BufReader, Read};
-use std::ops::Add;
+use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
-use std::process::exit;
-use nix::libc::printf;
+use prettytable::row;
+use clap::{self, Parser};
 
-// section table
+
 #[derive(Debug)]
 struct Table {
     map: HashMap<OsString, u64>
@@ -20,30 +16,21 @@ impl Table {
     fn new() -> Self {
         Self {map: HashMap::new()}
     }
-}
 
-impl Display for Table {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+    fn print_horizontal(&self, f: &mut Formatter) -> Result<(), Error> {
+        let mut table = prettytable::Table::new();
         let mut names = Vec::with_capacity(self.map.capacity());
-        let mut sizes= Vec::with_capacity(self.map.capacity());
+        let mut sizes = Vec::with_capacity(self.map.capacity());
 
         let mut intermediary = self.map.iter().collect::<Vec<(&OsString, &u64)>>();
-        // intermediary.sort_by(|(_,y1), (_,y2)| y2.cmp(y1));
-        intermediary.sort_by(|(x1,_), (x2,_)| x1.cmp(x2));
-
-        for row in intermediary {
-            names.push(format!(".{}", row.0.to_str().unwrap()));
-
-            let size = match u64::ilog10(row.1.to_owned()) / 3 {
-                0 => { format!("{}", row.1) }
-                1 => { format!("{}K", row.1/1000) }
-                2 => { format!("{}M", row.1/1000_000) }
-                _ => { format!("{}G", row.1/1000_000_000) }
-            };
+        intermediary.sort_by(|(x1, _), (x2, _)| x1.cmp(x2));
+        intermediary.iter().for_each(|row| {
+            let name = Self::format_name(&row);
+            let size = Self::format_size(&row);
+            names.push(name);
             sizes.push(size);
-        }
+        });
 
-        let mut table = prettytable::Table::new();
         table.add_row(names.into());
         table.add_row(sizes.into());
 
@@ -51,20 +38,77 @@ impl Display for Table {
 
         Ok(())
     }
+
+    fn print_vertical(&self, f: &mut Formatter) -> Result<(), Error> {
+        let mut table = prettytable::Table::new();
+        let mut intermediary = self.map.iter().collect::<Vec<(&OsString, &u64)>>();
+        intermediary.sort_by(|(x1, _), (x2, _)| x1.cmp(x2));
+        intermediary.iter().for_each(|row| {
+            let name = Self::format_name(row);
+            let size = Self::format_size(row);
+            table.add_row(row![name, size]);
+        });
+
+        write!(f, "{}", table.to_string())?;
+
+        Ok(())
+    }
+
+    fn format_name(row: &(&OsString, &u64)) -> String {
+        let name = (format!(".{}", row.0.to_str().unwrap()));
+        name
+    }
+
+    fn format_size(row: &(&OsString, &u64)) -> String {
+        let size = match u64::ilog10(row.1.to_owned()) / 3 {
+            0 => { format!("{}", row.1) }
+            1 => { format!("{}K", row.1 / 1000) }
+            2 => { format!("{}M", row.1 / 1000_000) }
+            _ => { format!("{}G", row.1 / 1000_000_000) }
+        };
+        size
+    }
 }
 
-//section mode
-#[derive(PartialEq)]
+impl Display for Table {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.print_vertical(f)
+    }
+}
+
+#[derive(PartialEq, Debug)]
 enum Mode {
     FileCount,
     CharCount,
     LineCount,
 }
 
-//section main
-fn main() {
 
-    let mode = Mode::CharCount;
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None,)]
+struct Args {
+    #[arg(long, short)]
+    file_count: bool,
+    #[arg(long, short)]
+    char_count: bool,
+    #[arg(long, short)]
+    line_count: bool,
+}
+
+
+fn main() {
+    let mut mode;
+    let args = Args::parse();
+    if args.file_count {
+        mode = Mode::FileCount;
+    } else if args.char_count {
+        mode = Mode::CharCount;
+    } else if args.line_count {
+        mode = Mode::LineCount;
+    } else {
+        mode = Mode::CharCount
+    }
+
     let mut table = Table::new();
 
     let mut files: Vec<PathBuf> = Vec::new();
@@ -86,19 +130,17 @@ fn main() {
                 } else if mode == Mode::FileCount{
                     table.map.insert(extension.to_owned().to_owned(), 1);
                 } else if mode == Mode::LineCount {
-                    *v = BufReader::new(File::open(file).unwrap()).lines().count() as u64;
+                    table.map.insert(extension.to_owned().to_owned(), BufReader::new(File::open(file).unwrap()).lines().count() as u64);
                 }
 
             }
         }
     }
 
+    println!("{:?}", mode);
     println!("{}", table);
 
-
-
 }
-//section recursive
 fn recursive_walk(dir: &Path, files: &mut Vec<PathBuf>) {
     for file in read_dir(dir).unwrap() {
         let path = file.unwrap().path();
